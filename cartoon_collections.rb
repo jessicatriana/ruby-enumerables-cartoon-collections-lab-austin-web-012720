5bc9c0ae883af577fa4027dda895793dbcd2d509
Jessica Triana <jessica.triana.tx@gmail.com>
2020-01-03 03:11:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 
 def find_the_cheese(array)   # returns the first element that is cheese
   cheese_types = ["cheddar", "gouda", "camembert"]  #the given array
-  array.find { |item| array.include?(cheese_types) } 
+  array.find { |item| cheese_types.include?(item) } 
 end
